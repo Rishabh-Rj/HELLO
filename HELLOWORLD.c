3e5ee4804bc5fa2e463a79ecb3a83ace74a601f0
Rishabh joshi <rishabhkv2014@gmail.com>
2017-09-17 23:34:26 +0530

    git bash change

--- HELLOWORLD.c
+++ HELLOWORLD.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
-#include conio.h>
-void main()
-{ clrscr();
+#include<iostream>
+a
+using namespace std;
+int main()
+{
   printf("nishant");
   printf("test1");
-  dffrdgdheh
-  getch();
+  return ;
+
+
 }
